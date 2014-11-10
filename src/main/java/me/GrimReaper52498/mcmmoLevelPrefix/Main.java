6c50d76b25db73c266725611f12cbb7e81b13416
GrimReaper52498 <tylerjbrady598@gmail.com>
2014-11-10 17:00:33 -0500

    Initial commit

---
+++ src/main/java/me/GrimReaper52498/mcmmoLevelPrefix/Main.java
@@ -0,0 +1,39 @@
+package me.GrimReaper52498.mcmmoLevelPrefix;
+
+import net.md_5.bungee.api.ChatColor;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.AsyncPlayerChatEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import com.gmail.nossr50.api.ExperienceAPI;
+
+public class Main extends JavaPlugin implements Listener
+{
+    
+    String brackets =  ChatColor.translateAlternateColorCodes('&', getConfig().getString("Bracket-Color"));
+    String num = ChatColor.translateAlternateColorCodes('&', getConfig().getString("Number-Color"));
+    
+    
+    
+    public void onEnable()
+    {
+	getConfig().options().copyDefaults(true);
+	saveDefaultConfig();
+
+	getServer().getPluginManager().registerEvents(this, this);
+    }
+
+    public void onDisable()
+    {
+
+	saveDefaultConfig();
+	
+    }
+
+    @EventHandler
+    public void onPlayerChat(AsyncPlayerChatEvent event)
+    {
+	event.setFormat(event.getFormat().replace("{LEVEL}", brackets + "[" + num + ExperienceAPI.getPowerLevel(event.getPlayer()) + brackets + "]"));
+    }
+}
